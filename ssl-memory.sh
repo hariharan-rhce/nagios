c29c764dc40ab4cb45ec8991b915d5cd063323ab
hari <hariharan@ephronsystems.com>
2019-07-05 11:14:34 +0530

    permission  commita

--- ssl-memory.sh
+++ ssl-memory.sh
@@ -4,12 +4,13 @@ yum install nagios-plugins-perl
 
 read -p 'Domain name: ' host
 
-cd /usr/lib64/nagios/plugins/ |  https://assets.nagios.com/downloads/nagiosxi/scripts/custom_check_mem 
+cd /usr/lib64/nagios/plugins/ | wget https://assets.nagios.com/downloads/nagiosxi/scripts/custom_check_mem 
 
 cd /usr/lib64/nagios/plugins/ |  wget https://raw.githubusercontent.com/hariharan-rhce/nagios/master/check_ssl_certificate
 
+cd /usr/lib64/nagios/plugins/ | mv /usr/lib64/nagios/plugins/custom_check_mem /usr/lib64/nagios/plugins/check_mem
 chmod 755 /usr/lib64/nagios/plugins/check_ssl_certificate
-chmod 755/usr/lib64/nagios/plugins/custom_check_mem
+chmod 755 /usr/lib64/nagios/plugins/custom_check_mem
 
 cat <<EOT >> /etc/nagios/nrpe.cfg
 command[check_ssl_certificate]=/usr/lib64/nagios/plugins/check_ssl_certificate -H $host -c 30 -w 60 -v
