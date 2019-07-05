061f0259ef72b7b2ea617b3b41e01cba7ca89514
hari <hariharan@ephronsystems.com>
2019-07-05 11:31:16 +0530

    start services commit

--- ssl-memory.sh
+++ ssl-memory.sh
@@ -19,3 +19,5 @@ cat <<EOT >> /etc/nagios/nrpe.cfg
 command[check_ssl_certificate]=/usr/lib64/nagios/plugins/check_ssl_certificate -H $host -c 30 -w 60 -v
 command[check_mem]=/usr/lib64/nagios/plugins/check_mem -w 80 -c 90
 EOT
+
+/etc/init.d/nrpe restart
